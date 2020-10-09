c8b144480d93bf42187f71af2c86cd727113dd20
Jackson Krauss <jacksonkrauss1@gmail.com>
2020-10-09 15:25:07 -0500

    grid watchlist screen

--- Cinematch/WatchlistGridViewController.swift
+++ Cinematch/WatchlistGridViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-class WatchlistGridViewController: UIViewController {
+class WatchlistGridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath)
+        return cell
+    }
+    
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var listButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionView.delegate = self
         // Do any additional setup after loading the view.
     }
     
