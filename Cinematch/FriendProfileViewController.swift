d9b4c846e790f019982c847eed18399e07054bae
Jackson Krauss <jacksonkrauss1@gmail.com>
2020-10-09 11:35:12 -0500

    screens created

---
+++ Cinematch/FriendProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FriendProfileViewController.swift
+//  Cinematch
+//
+//  Created by Jackson Krauss on 10/9/20.
+//
+
+import UIKit
+
+class FriendProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
