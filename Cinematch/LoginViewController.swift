7a34d5043e227fa9522e69f9d59455aebe6e4a6b
mparfan <32278807+mparfan@users.noreply.github.com>
2020-10-20 01:30:20 -0500

    added login/signup functionality

--- Cinematch/LoginViewController.swift
+++ Cinematch/LoginViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    let ref = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +21,26 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginDidPress(_ sender: Any) {
+        guard let username = usernameTextField.text,
+              let password = passwordTextField.text
+        else {
+            return
+        }
+        
+        ref.child("user_info").child(username).observeSingleEvent(of: .value, with: {
+            snapshot in
+            if !snapshot.exists() {
+                return
+            }
+            if let email = snapshot.childSnapshot(forPath: "email").value as? String {
+                Auth.auth().signIn(withEmail: email, password: password) {
+                    user, error in
+                    if error == nil {
+                        self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                    }
+                }
+            }
+        })
     }
-    */
-    
-    // need segue for login button
 }
