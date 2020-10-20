31162559ae58dcb13e87bd88b4b941ce4ad71361
mparfan <32278807+mparfan@users.noreply.github.com>
2020-10-20 00:52:44 -0500

    Revert "Integrating Login/Sign Up Functionality"

--- Cinematch/SignUpViewController.swift
+++ Cinematch/SignUpViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Firebase
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var firstNameTextField: UITextField!
@@ -16,8 +15,6 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     
-    let ref = Database.database().reference()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,33 +22,16 @@ class SignUpViewController: UIViewController {
     }
     
 
-    @IBAction func signUpDidPress(_ sender: Any) {
-        // later: maybe an alert to confirm everything is correct would be good
-        guard let firstName = firstNameTextField.text,
-              let lastName = lastNameTextField.text,
-              let username = usernameTextField.text,
-              let email = emailTextField.text,
-              let password = passwordTextField.text,
-              let confirmedPassword = confirmPasswordTextField.text
-        else {
-            return
-            // need status label bar to explain why sign up failed
-        }
-        
-        // need something to check if passwords are equal, label for mismatching passwords
-        
-        let newUserInfo = [
-            "first_name": firstName,
-            "last_name": lastName,
-            "email": email]
-        let newUser = self.ref.child("user_info").child(username)
-        newUser.setValue(newUserInfo)
-        print("Data updated")
-        
-        Auth.auth().createUser(withEmail: email, password: password) { user, error in
-            if error == nil {
-                Auth.auth().signIn(withEmail: email, password: password)
-            }
-        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
+    
+    // need sign-up button segue
+
 }
