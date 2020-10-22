8eba9bef72617fefd9a40534f069872c71f92c17
mparfan <32278807+mparfan@users.noreply.github.com>
2020-10-22 13:54:11 -0500

    did search bar touch-ups and segues

--- Cinematch/SearchViewController.swift
+++ Cinematch/SearchViewController.swift
@@ -27,11 +27,21 @@ class SearchViewController: UIViewController, UITableViewDataSource, UISearchBar
         // initial data source
         filteredMovies = moviesData
         filteredUsers = usersData
+        
+        searchTableView.rowHeight = 166
     }
     
     @IBAction func filterSelected(_ sender: Any) {
         // table view needs to be updated
         searchTableView.reloadData()
+        switch searchTypeSegCtrl.selectedSegmentIndex {
+            case 0:
+                searchTableView.rowHeight = 166
+            case 1:
+                searchTableView.rowHeight = 115
+            default:
+                searchTableView.rowHeight = 166
+        }
     }
     
     // get count of table cells based on selected filter
@@ -51,18 +61,18 @@ class SearchViewController: UIViewController, UITableViewDataSource, UISearchBar
         switch searchTypeSegCtrl.selectedSegmentIndex {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieSearchTableViewCell
-            var currentMovie = filteredMovies[indexPath.row]
+            let currentMovie = filteredMovies[indexPath.row]
             cell.movieTitleLabel?.text = currentMovie.title!
             cell.movieRatingLabel?.text = currentMovie.rating!
-            var poster = UIImageView()
-            poster.load(url: URL(string: "https://image.tmdb.org/t/p/original" + currentMovie.poster!)!)
-            cell.moviePosterImageView? = poster
+            cell.moviePosterImageView.load(url: URL(string: "https://image.tmdb.org/t/p/original" + currentMovie.poster!)!)
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath) as! PeopleSearchTableViewCell
-            var currentPerson = filteredUsers[indexPath.row]
+            let currentPerson = filteredUsers[indexPath.row]
             cell.nameLabel?.text = currentPerson.name
             cell.usernameLabel?.text = currentPerson.username
+            cell.profilePicImageView.image = currentPerson.profilePicture
+            cell.profilePicImageView.layer.cornerRadius = cell.profilePicImageView.frame.height / 2
             return cell
         default:
             return UITableViewCell()
@@ -88,5 +98,18 @@ class SearchViewController: UIViewController, UITableViewDataSource, UISearchBar
             break
         }
         searchTableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "movieSearchSegue" {
+            // "Cannot complete segue at this time, as movie detail view needs a swipe delegate"
+            // DO NOT CLICK
+        }
+        if segue.identifier == "peopleSearchSegue" {
+            let indexPath = searchTableView.indexPathForSelectedRow
+            let selectedUser = usersData[indexPath!.row]
+            let destination = segue.destination as! FriendProfileViewController
+            destination.user = selectedUser
+        }
     }
 }
